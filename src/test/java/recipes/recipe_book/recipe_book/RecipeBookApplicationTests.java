7252ee17d933df082212c02d27b5870739bb1ba0
zikit <zikit.start@yandex.ru>
2022-12-17 14:00:15 +0300

    Первый коммит.

---
+++ src/test/java/recipes/recipe_book/recipe_book/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package recipes.recipe_book.recipe_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
